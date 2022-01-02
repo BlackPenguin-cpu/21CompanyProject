9ee7aca0156d09de4334e4b30b25f253cbfafbee
limjiho <ljiho1208@gmail.com>
2022-01-03 00:35:09 +0900

    비둘기 완성
    충돌나면 찬우탓

---
+++ Assets/MiniGame/jiho/script/lifeline.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifeline : MonoBehaviour
+{
+    public GameObject level;
+    public int life = 1;
+    void Start()
+    {
+        maxlife();
+    }
+
+    void Update()
+    {
+        if(life == 0)//게임 클리어
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    void maxlife()
+    {
+        if (level.GetComponent<timer>().level == 1)
+        {
+            life = 4;
+        }
+        else if (level.GetComponent<timer>().level == 2)
+        {
+            life = 7;
+        }
+        else if (level.GetComponent<timer>().level == 3)
+        {
+            life = 10;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Pigeon")
+        {
+            life--;
+            Destroy(collider.gameObject);
+        }
+    }
+}
