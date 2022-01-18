e821c8520bbb02afbe2d8d15662382614c3d6cb3
BlackPenguin <dkstmdwnswns@naver.com>
2022-01-19 08:43:25 +0900

    UIManager 입포트

---
+++ Assets/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public Image[] Life;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
