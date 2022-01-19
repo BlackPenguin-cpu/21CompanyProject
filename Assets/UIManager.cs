af062a2b7efe672d7d3f58183b3b4398e77f866b
BlackPenguin <dkstmdwnswns@naver.com>
2022-01-19 16:55:55 +0900

    작업중 풀 받지마셈

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 using DG.Tweening;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
     public Image[] Life;
+    public Image BackGround;
+    public TextMeshProUGUI Score;
+    public TextMeshProUGUI Combo;
+    public TextMeshProUGUI AnimalAll;
+    public TextMeshProUGUI AnumalNews;
 
     void Start()
     {
@@ -17,4 +24,28 @@ public class UIManager : MonoBehaviour
     {
 
     }
+
+    void ScreenPrint(bool isWin)
+    {
+        if (isWin)
+        {
+            BackGround.color = new Color(0, 1, 0, 1);
+        }
+        else
+        {
+            BackGround.color = new Color(0, 1, 0, 1);
+
+        }
+    }
+
+    public IEnumerator ClearTextEffect(int ScoreValue, int AddValue, float Delay)
+    {
+        int targetValue = ScoreValue + AddValue;
+        float Value = AddValue / Delay;
+        while (targetValue.ToString() == Score.text)
+        {
+            ScoreValue = (int)(Value * Time.deltaTime);
+        }
+    }
+
 }
